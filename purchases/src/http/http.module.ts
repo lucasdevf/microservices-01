f62fb57fd642f3bd01243dce8847a30d2d4a0963
Lucas Ferreira <lucas.ferreira@ghap.com.br>
2022-08-17 20:10:22 -0300

    feat/purchases: list all purchases

--- purchases/src/http/http.module.ts
+++ purchases/src/http/http.module.ts
@@ -8,6 +8,8 @@ import path from 'node:path';
 import { ApolloDriver } from '@nestjs/apollo';
 import { ProductsResolver } from './graphql/resolvers/products.resolver';
 import { ProductsService } from '../services/products.service';
+import { PurchasesResolver } from './graphql/resolvers/purchases.resolver';
+import { PurchasesService } from '../services/purchases.service';
 
 @Module({
   imports: [
@@ -18,6 +20,14 @@ import { ProductsService } from '../services/products.service';
       autoSchemaFile: path.resolve(process.cwd(), 'src/schema.gql'),
     }),
   ],
-  providers: [ProductsResolver, ProductsService],
+  providers: [
+    // Products
+    ProductsResolver,
+    ProductsService,
+
+    // Purchases
+    PurchasesResolver,
+    PurchasesService,
+  ],
 })
 export class HttpModule {}
