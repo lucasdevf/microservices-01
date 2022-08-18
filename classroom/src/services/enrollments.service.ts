a6a2935ad6fef715d0d8884f6da82e52042c9755
Lucas Ferreira <lucas.ferreira@ghap.com.br>
2022-08-18 00:13:50 -0300

    feat/classroom: get course by id with validation if user have access to course

--- classroom/src/services/enrollments.service.ts
+++ classroom/src/services/enrollments.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../database/prisma/prisma.service';
 
+interface GetByCourseAndSutdentIdParams {
+  courseId: string;
+  studentId: string;
+}
+
 @Injectable()
 export class EnrollmentsService {
   constructor(private prisma: PrismaService) {}
+
+  getByCourseAndStudentId({
+    courseId,
+    studentId,
+  }: GetByCourseAndSutdentIdParams) {
+    return this.prisma.enrollment.findFirst({
+      where: {
+        courseId,
+        studentId,
+        canceledAt: null,
+      },
+    });
+  }
 
   listAllEnrollments() {
     return this.prisma.enrollment.findMany({
